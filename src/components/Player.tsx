72dc32089a26c5bfa26031bb613aeef94186618f
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-05-03 18:51:19 +0200

    Create Player Component - WIP
    Just created Player Component

---
+++ src/components/Player.tsx
@@ -0,0 +1,19 @@
+interface Player { 
+	topPosition: number;
+	leftPosition: number;
+	pixelSize: number; 
+}
+
+export default function Player({topPosition, leftPosition, pixelSize} : Player) : JSX.Element {
+	return (
+		<div className="player"
+			style={
+				{
+					height: `${pixelSize}px`,
+					width: `${pixelSize}px`,
+					top: `${topPosition}px`,
+					left: `${leftPosition}px`,
+				}}>
+
+		</div>)
+}
