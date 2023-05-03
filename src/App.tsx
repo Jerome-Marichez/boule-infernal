6638ca383ff39c1324306383758b415b389141ab
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-05-03 11:29:56 +0200

    Init Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game from './scene/Game';
 
 function App() {
   return (
-    <div className="App">
-        
-    </div>
+  
+      <Game />
+  
   );
 }
 
