5e919f433f677d5ec41cb707f717b4d08039ee29
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-06-20 15:12:05 +0200

    Drop fireEvent
    Drop fireEvent to move to a more modern way to use Keyboard though userEvent.

--- src/tests/components/MusicLoop.test.tsx
+++ src/tests/components/MusicLoop.test.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import { screen, render, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import { screen, render, act } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { MusicLoop } from '../../components';
 
 describe('MusicLoop component', () => {
 
 	test('renders audio after user interaction (arrow key press)', async () => {
-		const { rerender } = render(
-			<MusicLoop music="gameover" mute={true} />
-		);
-		
+		render(<MusicLoop music="gameover" mute={true} />);
+
 		let audioTag: HTMLElement | null = screen.queryByTestId('audio');
 		expect(audioTag).toBeNull();
 
-		fireEvent.keyDown(window, { key: 'ArrowUp' });
+		const user = userEvent.setup();
 
-		rerender(<MusicLoop music="gameover" mute={true} />);
+		await act(async () => {
+			await user.keyboard('[ArrowUp]');
+			await user.keyboard('[ArrowDown]');
+		});
+
 		audioTag = await screen.findByTestId('audio');
 		const sourceElement: HTMLElement = await screen.findByTestId('source');
 
