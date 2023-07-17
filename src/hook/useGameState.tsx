23d4b27976b5453f9e94e7d90685c246dc436f80
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-07-17 10:38:03 +0200

    Add Speed Velocity to hook useGameState
    Add Speed Velocity to hook useGameState

--- src/hook/useGameState.tsx
+++ src/hook/useGameState.tsx
@@ -5,19 +5,19 @@ import { setGameOver, setMaxScore, setScore, setSpeed } from "../redux/gameSlice
 
 
 /**
- * 
  * @param block The type of block that was hit: "empty | green | wall | goal"
  * @param numberGoal The number goal in the Level
+ * @param speedVelocity The speed velocity increase each time a level is completed
  * @returns A hook that updates Score, GameOver, and Speed when (Level completed) using Redux as a dependency.
  */
-export default function useGameState(block: "empty" | "green" | "wall" | "goal", numberGoal: number) {
+export default function useGameState(block: "empty" | "green" | "wall" | "goal", numberGoal: number, speedVelocity: number) {
 
 	const dispatch = useDispatch();
 	const score = useSelector((state: rootState) => state.gameState.score)
 	const maxScore = useSelector((state: rootState) => state.gameState.maxScore)
 	const gameOver = useSelector((state: rootState) => state.gameState.gameOver)
 	const speed = useSelector((state: rootState) => state.gameState.speed)
-
+	
 
 	useEffect(() => {
 		switch (block) {
@@ -30,7 +30,7 @@ export default function useGameState(block: "empty" | "green" | "wall" | "goal",
 			default:
 				if (score === numberGoal + maxScore) {
 					dispatch(setMaxScore(maxScore + numberGoal))
-					dispatch(setSpeed(speed - 3))
+					dispatch(setSpeed(speed - speedVelocity))
 				}
 				break;
 		}	
